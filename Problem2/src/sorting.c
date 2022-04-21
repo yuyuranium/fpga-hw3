9b272de353273851437c46528cbc9c93e1e897c8
japoka410666 <1026jason@gmail.com>
2022-04-21 16:44:51 +0800

    improve non-numeric error detection in sorting.c

--- Problem2/src/sorting.c
+++ Problem2/src/sorting.c
@@ -5,11 +5,13 @@
  */
 
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 #include "xparameters.h"
 #include "xgpio.h"
 #include "xil_printf.h"
 
-#define SIZE 20 
+#define SIZE 5
 #define SW_DEVICE_ID  XPAR_GPIO_0_DEVICE_ID
 
 XGpio SW_Gpio;
@@ -27,7 +29,7 @@ void Sorting(int* data,int mode)
 		key = data[i];
 		for(j = i-1; j>=0; j--)
 		{
-			if(!((key > data[j]) ^ mode))
+			if(!((key>data[j])^mode))
 				data[j+1] = data[j];
 			else
 				break;
@@ -35,10 +37,12 @@ void Sorting(int* data,int mode)
 		data[j+1] = key;
 	}
 }
+
 int main()
 {
 	int SW_Status;
 	int data[SIZE];
+	char str[100];
 	u32 sw_data;
 	/* Initialize the GPIO driver */
 	SW_Status = XGpio_Initialize(&SW_Gpio, SW_DEVICE_ID);
@@ -55,22 +59,34 @@ int main()
 		/* Scan input data*/
 		for(int i=0; i<SIZE; i++)
 		{
-			printf("Enter the No.%d Data:", i);
-			scanf("%d", &data[i]);
-			printf(" %d\r\n", data[i]);
-			printf("\r\n");
+			printf("Enter the No.%d Data:", i+1);
+			scanf("%s", str);
+			printf("%s\r\n", str);
+			data[i] = atoi(str);
+			
+			/*non-numeric-input-error detection*/
+			for(int j=0; j<strlen(str); j++)
+			{
+				if(!isdigit(str[j]))
+				{
+					printf("Please re-enter a numeric input.\r\n");
+					data[i--] = 0;
+					break;
+				}
+			}
+
 		}
 		/* Sorting Process*/
 		sw_data = XGpio_DiscreteRead(&SW_Gpio, 1);
 		xil_printf("switches data = %d\r\n", sw_data);
 		Sorting(data, sw_data);
 		/* Show the Result*/
-		printf("\r\nRESULT:");
+		printf("RESULT:");
 		for(int i=0; i<SIZE; i++)
 		{
 			printf(" %d", data[i]);
 		}
-		printf("\r\n");
+		printf("\r\n\r\n");
 	};
 	xil_printf("Successfully ran Gpio Example\r\n");
 	return XST_SUCCESS;
