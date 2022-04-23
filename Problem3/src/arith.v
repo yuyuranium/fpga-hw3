2646dae0cb270308a9ac83ddfc5f3da91b05a8b2
yuyuranium <e24076459@gs.ncku.edu.tw>
2022-04-24 01:09:56 +0800

    Modify IO port names of arith.v

--- Problem3/src/arith.v
+++ Problem3/src/arith.v
@@ -3,17 +3,17 @@ module arith #(
   Sub = 1,
   Mul = 2
 ) (
-  input  signed [7:0] opd1,
-  input  signed [7:0] opd2,
-  input         [1:0] op,
-  output reg    [7:0] res
+  input  signed [7:0] opd1_i,
+  input  signed [7:0] opd2_i,
+  input         [1:0] op_i,
+  output reg    [7:0] res_o
 );
 
 always @(*) begin
-  case (op)
-    Add: res = opd1 + opd2;
-    Sub: res = opd1 - opd2;
-    Mul: res = opd1 * opd2;
+  case (op_i)
+    Add: res_o = opd1_i + opd2_i;
+    Sub: res_o = opd1_i - opd2_i;
+    Mul: res_o = opd1_i * opd2_i;
   endcase
 end
 
