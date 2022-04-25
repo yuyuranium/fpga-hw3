59dbcb1b98bd95c1b93f12d6922bfcc30815a680
WillyChennnn <willychen1210@gmail.com>
2022-04-25 14:47:23 +0800

    modify driver for mask problem

--- Problem3/p3-1/driver/p3_1_arith.c
+++ Problem3/p3-1/driver/p3_1_arith.c
@@ -8,7 +8,7 @@ u32 arith(UINTPTR baseAddr, u32 opd1, u32 opd2, u32 arith_mode)
 
 	u32 opd1_mask = opd1 & 0xff; //mask to 8 bits
 	u32 opd2_mask = opd2 & 0xff; //mask to 8 bits
-	u32 arith_mode_mask = arith_mode & 0x11; //mask to 2 bits
+	u32 arith_mode_mask = arith_mode & 0x3; //mask to 2 bits
 
 	u32 data = opd1_mask + (opd2_mask << 8) + (arith_mode_mask << 16);
 	P3_1_ARITH_mWriteReg(baseAddr, 0, data);
