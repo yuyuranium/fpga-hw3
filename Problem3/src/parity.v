bd06d41bac554321b0d0f09876b202dda105024a
yuyuranium <e24076459@gs.ncku.edu.tw>
2022-04-25 10:43:09 +0800

    Remove odd_parity

--- Problem3/src/parity.v
+++ Problem3/src/parity.v
@@ -5,11 +5,9 @@
 
 module parity (
   input [31:0] data_i,
-  output       odd_parity_o,
   output       even_parity_o
 );
 
   assign even_parity_o = ^data_i;
-  assign odd_parity_o  = ~even_parity_o;
 
 endmodule
