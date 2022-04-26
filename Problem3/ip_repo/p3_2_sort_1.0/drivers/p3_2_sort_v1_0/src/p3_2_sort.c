5a070855c1672ba14ba6ba8b1f59893daaaf6c38
yuyuranium <e24076459@gs.ncku.edu.tw>
2022-04-26 12:08:06 +0800

    Add some thoughts on driver codes

--- Problem3/ip_repo/p3_2_sort_1.0/drivers/p3_2_sort_v1_0/src/p3_2_sort.c
+++ Problem3/ip_repo/p3_2_sort_1.0/drivers/p3_2_sort_v1_0/src/p3_2_sort.c
@@ -3,4 +3,24 @@
 /***************************** Include Files *******************************/
 #include "p3_2_sort.h"
 
+/* My thought on the driver code, feel free to remove these comments once done -- by yuyuranium */
+/* void sort(unsigned char *arr)
+ * {
+ *   loop i = 1 ~ 4:
+ *     assemble 4 elements in arr into a u32
+ *     write the u32 to the memory-mapped register[i] via AXI 
+ *   write 1 to reg0
+ *
+ *   while (reg0 == 1):
+ *     read reg0
+ *
+ *   loop i = 1 ~ 4:
+ *     retrieve u32 data via AXI and store to temp
+ *     loop j = 1 ~ 4:
+ *       arr[i * 4 + j] = temp & 0xff
+ *       temp = temp >> 8
+ * }
+ *
+ */
+
 /************************** Function Definitions ***************************/
